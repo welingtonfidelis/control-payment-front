9e23eea301485d72c204454f97306f75231951c8
Welington Fidelis <welingtonfidelis@gmail.com>
2020-06-24 12:04:29 -0300

    Updates

---
+++ precache-manifest.a6c18dd8b5667652ea458141e9270120.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c54be19907441ceb1626bb9e61c2e597",
+    "url": "/control-payment-front/index.html"
+  },
+  {
+    "revision": "f3e7f280022bfa6ea7fe",
+    "url": "/control-payment-front/static/css/main.94e44302.chunk.css"
+  },
+  {
+    "revision": "96d65a7ec98835e33ec7",
+    "url": "/control-payment-front/static/js/2.bba6a9ec.chunk.js"
+  },
+  {
+    "revision": "f3e7f280022bfa6ea7fe",
+    "url": "/control-payment-front/static/js/main.b3f416e2.chunk.js"
+  },
+  {
+    "revision": "581688175e8fba6085ce",
+    "url": "/control-payment-front/static/js/runtime-main.cbde22fe.js"
+  },
+  {
+    "revision": "9c8c5848e353d115ec66b9f1ce8aa297",
+    "url": "/control-payment-front/static/media/cashregister_in.9c8c5848.png"
+  },
+  {
+    "revision": "24eb8f1a79420d71c03a93a4afa4272f",
+    "url": "/control-payment-front/static/media/cashregister_out.24eb8f1a.png"
+  }
+]);
